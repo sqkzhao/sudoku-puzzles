1c933dfa12feec79d6d7244308a62a646d9a4342
Kay Zhao <58795757+sqkzhao@users.noreply.github.com>
2020-10-28 13:35:33 -0700

    Update settings.py

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%ow(q1gcurz-06*1&+pnr#_@*u#5co)-@nvx=!prsi4qgu#*3-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
